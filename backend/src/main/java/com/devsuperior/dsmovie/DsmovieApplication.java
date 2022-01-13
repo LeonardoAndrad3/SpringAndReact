b56b3393b59ddd0eec16a98ff81801db4b421a46
leobiriba <leonardoxcontato@gmail.com>
2022-01-13 13:59:38 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/devsuperior/dsmovie/DsmovieApplication.java
@@ -1,7 +1,5 @@
 package com.devsuperior.dsmovie;
 
-import java.util.Scanner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,8 +9,6 @@ public class DsmovieApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DsmovieApplication.class, args);
 		
-		Scanner sc = new Scanner(System.in);
-	
-		sc.close();
+			
 	}
 }
